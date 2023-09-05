84478b2b1b209ecb81bf351f8fcb6305975da8b2
ArttuBrotherus <44648394+ArttuBrotherus@users.noreply.github.com>
2023-09-05 19:24:11 +0300

    installed express, it's in use

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,4 +1,6 @@
-const http = require('http')
+
+const express = require('express')
+const app = express()
 
 let persons = [
     { 
@@ -21,13 +23,17 @@ let persons = [
         "name": "Mary Poppendieck", 
         "number": "39-23-6423122"
       }
-  
-  ]
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(persons))
-  })
-  
+]
+
+app.get('/', (request, response) => {
+  response.send(persons)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
