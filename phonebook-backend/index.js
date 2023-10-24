afef831486fb3023484868eb79ed1277afc4bbb7
ArttuBrotherus <44648394+ArttuBrotherus@users.noreply.github.com>
2023-10-24 12:16:18 +0300

    Ex. 3.15 and 3.16 and 3.21

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -22,17 +22,38 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
+
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
+
+    if(person){
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+
   })
+
+  .catch(error => next(error))
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  // persons = persons.filter(p => p.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
